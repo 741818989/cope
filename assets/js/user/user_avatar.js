a9ee97612ee4062bb320c5d6d76ea590f0438ba9
gaowei <gaowei@itcast.cn>
2020-10-14 16:36:18 +0800

    大事件

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -1,18 +1,18 @@
 $(function() {
-  var layer = layui.layer
+ var layer = layui.layer
 
-  // 1.1 获取裁剪区域的 DOM 元素
-  var $image = $('#image')
-  // 1.2 配置选项
-  const options = {
-    // 纵横比
-    aspectRatio: 1,
-    // 指定预览区域
-    preview: '.img-preview'
-  }
+ //1.1获取裁剪区域的DOM元素
+ var $image = $('#image')
+ //1.2配置选项
+ const options = {
+   //纵横比
+   aspectRatio:1,
+   //指定预览区域
+   preview:'.img-preview'
+ }
 
-  // 1.3 创建裁剪区域
-  $image.cropper(options)
+ //1.3 创建裁剪区域
+ $image.cropper(options)
 
   // 为上传按钮绑定点击事件
   $('#btnChooseImage').on('click', function() {
